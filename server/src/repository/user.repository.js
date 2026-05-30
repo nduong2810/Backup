60dd7508161498546fada099980e612933e40c38
nduong2810 <duongboil5@gmail.com>
2026-05-30 21:10:01 +0700

    Add user lookup helpers for legacy authors

--- server/src/repository/user.repository.js
+++ server/src/repository/user.repository.js
@@ -1,5 +1,7 @@
 import User from '../model/user.model.js';
 
+const escapeRegex = (value = '') => value.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+
 class UserRepository {
     async findByEmail(email) {
         return await User.findOne({ email });
@@ -7,6 +9,13 @@ class UserRepository {
 
     async findById(userId) {
         return await User.findById(userId).select('-password');
+    }
+
+    async findByFullName(fullName) {
+        if (!fullName) return null;
+        return await User.findOne({
+            fullName: new RegExp(`^${escapeRegex(fullName.trim())}$`, 'i'),
+        }).select('-password');
     }
 
     async updateUserByEmail(email, updateData) {
