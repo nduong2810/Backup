f0acdbfbc80b67938399630c688f026dc00dc094
nduong2810 <duongboil5@gmail.com>
2026-05-30 20:05:26 +0700

    Include author id for donation comments

--- server/src/repository/comment.repository.js
+++ server/src/repository/comment.repository.js
@@ -10,13 +10,13 @@ class CommentRepository {
     // Populate author info + sắp xếp cũ nhất trước (dòng thời gian)
     async findByPostId(postId) {
         return await Comment.find({ post: postId })
-            .populate('author', 'fullName avatar')
+            .populate('author', '_id fullName avatar email')
             .sort({ createdAt: 1 });  // Comment cũ nhất lên trước
     }
 
     async findById(commentId) {
         return await Comment.findById(commentId)
-            .populate('author', 'fullName avatar major')
+            .populate('author', '_id fullName avatar major email')
             .populate('post', 'title author');
     }
 
