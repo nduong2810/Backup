2f2b3c2f39f463d42485379246820c52f5a26326
nduong2810 <duongboil5@gmail.com>
2026-05-31 14:38:24 +0700

    Attach access token to API requests

--- client/src/lib/apiClient.js
+++ client/src/lib/apiClient.js
@@ -14,4 +14,15 @@ const apiClient = axios.create({
   },
 });
 
+apiClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem('accessToken');
+
+  if (token) {
+    config.headers = config.headers || {};
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 export default apiClient;
